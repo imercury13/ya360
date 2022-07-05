234f0bbda9dd7e37fe39d9d2521e4db823318e4a
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-07-05 08:52:57 +0300

    Исправления в meta

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ setup(
     version=ya360.__version__,
     packages=find_packages(),
     description='ya360 - Yandex 360 admin cli tool',
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
     author='Igor Kuptsov',
     author_email='i.kuptsov@uh.net.ru',
-    url='https://github.com/imercury13/ya360',
+    maintainer='Igor Kuptsov',
+    maintainer_email='i.kuptsov@uh.net.ru',
+    download_url='https://github.com/imercury13/ya360',
+    url='https://imercury13.github.io/ya360',
     license='GPL-3.0',
     entry_points={
         'console_scripts':
