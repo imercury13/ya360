c12daf033b3465ffa6ec1fe5846a3ed1cb918e7a
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-07-22 15:48:13 +0300

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
-    html_theme = 'default'
+    html_theme = 'sphinx_rtd_theme'
 else:
     html_theme = 'sphinx_rtd_theme'
 
@@ -74,4 +74,4 @@ exclude_patterns = []
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
