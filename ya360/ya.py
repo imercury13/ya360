eb6d02d903c7570257271478a4e427bb7ad15d2a
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-08-01 10:37:45 +0300

    show user

--- ya360/ya.py
+++ ya360/ya.py
@@ -354,14 +354,12 @@ def show_user(args):
 	check_request(ds)
 
 	print('{:>20s} {:<17s}'.format('ID:',ds['id']))
-	print('{:>20s} {:<17s}'.format('Nickname:',ds['nickname']))
+	print('{:>20s} {:<17s}'.format('Nickname (Login):',ds['nickname']))
 	print('{:>20s} {:<17s}'.format('Ф.И.О.:',ds['name']['last']+' '+ds['name']['first']+' '+ds['name']['middle']))
 	print('{:>20s} {:<17s}'.format('Должность:',ds['position']))
-	print('{:>20s} {:<17d}'.format('ID подразделения:',ds['departmentId']))
-	gr = " ".join(str(x) for x in ds['groups'])
-	print('{:>20s} {:<17s}'.format('ID групп:', gr))
+	print('{:>20s} {:<17s}'.format('Подразделение:',ya360.show_department(__token__, __orgID__, str(ds['departmentId']))['name']))
 	print('{:>20s} {:<17s}'.format('E-mail:', ds['email']))
-	al = " ".join(str(x) for x in ds['aliases'])
+	al = ", ".join(str(x) for x in ds['aliases'])
 	print('{:>20s} {:<17s}'.format('Альясы:', al))
 	if ds['isAdmin']:
 		print('{:>20s} {:<17s}'.format('Администратор:', 'Да'))
@@ -379,6 +377,17 @@ def show_user(args):
 		print('{:>20s} {:<17s}'.format('Форма жизни:', 'Робот'))
 	else:
 		print('{:>20s} {:<17s}'.format('Форма жизни:', 'Живой человек'))
+	gr = " ".join(str(x) for x in ds['groups'])
+	print('{:>20s} {:<17s}'.format(' Состоит в группах:', ''))
+	for group in ds['groups']:
+		#print(str(ya360.show_group(__token__, __orgID__, str(group))['label']))
+		#try:
+		name = ya360.show_group(__token__, __orgID__, str(group))
+		check_request(name)
+		print(f'{"":>20} {name["name"]:<17s}')
+		#except Exception as e:
+		#	print(f'{"":>20} {str(ya360.show_group(__token__, __orgID__, str(group))["id"]):<17s} - {e}')
+		#print('{:>20} {:<17}'.format('', str(ya360.show_group(__token__, __orgID__, group['name'])['label'])))
 	print('{:>20s} {:<17s}'.format('Контакты:', ''))
 	for cn in ds['contacts']:
 		print('{:>20} {:>15}: {}'.format('',cn['type'],cn['value']))
