e89547be3f99fc4926ffda4782840e92cf470330
Igor Kuptsov <i.kuptsov@uh.net.ru>
2022-08-29 10:45:25 +0300

    load_whitelist

--- ya360/antispam.py
+++ ya360/antispam.py
@@ -4,12 +4,17 @@ from .tid import load_token, load_orgID
 from .whois import check_request
 from yandex_360 import ya360, antispam
 
-def show_whitelist():
+def load_whitelist():
     '''Функция вывода содержимого белого списка'''
 
     __token__ = load_token()
     __orgID__ = load_orgID()
     
-    wl = check_request(antispam.show_whitelist(__token__, __orgID__))
+    return check_request(antispam.show_whitelist(__token__, __orgID__))
 
-    print(wl)
+
+def show_whitelist():
+    '''Функция вывода содержимого белого списка'''
+
+    print(load_whitelist())
+
