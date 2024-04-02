2f6a45e47c7c743d160a2d6f37798c3a63b19561
Купцов Игорь Валерьевич <i.kuptsov@norgau.com>
2024-04-02 09:32:18 +0300

    add disk log

--- ya360/cmd.py
+++ ya360/cmd.py
@@ -12,7 +12,7 @@ from .users import show_users, show_user, update_user, create_user, add_alias_us
 from .groups import create_group, delete_group, update_group, add_member_group, delete_member_group, show_group, show_groups
 from .mail import edit_access_mailbox, delete_access_mailbox, show_status_access_mailbox, show_access_mailbox_user, show_users_access_mailbox
 from .whois import whois
-from .logs import show_mail_log
+from .logs import show_mail_log, show_disk_log
 from .configure import make_config
 from .antispam import show_whitelist, add_in_whitelist, remove_from_whitelist, delete_whitelist
 from .routing import add_in_routing, show_routing, remove_from_routing
@@ -201,6 +201,12 @@ def gen_parser():
     parser_logs_comm.add_argument('--csv', type=str, help='Выгрузить в CSV файл')
 
     parser_logs_comm = subparser_logs.add_parser('disk',help='Аудит-лог диска')
+    parser_logs_comm.add_argument('--beforeDate', type=str, help='Верхняя граница периода выборки в формате ISO 8601')
+    parser_logs_comm.add_argument('--afterDate', type=str, help='Нижняя граница периода выборки в формате ISO 8601')
+    parser_logs_comm.add_argument('--includeUsers', type=str, help='Список пользователей, действия которых должны быть включены в список событий')
+    parser_logs_comm.add_argument('--excludeUsers', type=str, help='Список пользователей, действия которых должны быть исключены из списка событий')
+    parser_logs_comm.add_argument('--types', type=str, help='Типы событий которые должны быть включены в список. По умолчанию включаются все события')
+    parser_logs_comm.add_argument('--csv', type=str, help='Выгрузить в CSV файл')
 
     parser_antispam = subparsers.add_parser('antispam', help='Антиспам')
     subparser_antispam = parser_antispam.add_subparsers(dest='sub_com_antispam')
@@ -312,6 +318,8 @@ def start():
     if args.sub_com == 'logs':
         if args.sub_com_logs == 'mail':
             show_mail_log(args)
+        if args.sub_com_logs == 'disk':
+            show_disk_log(args)
 
     if args.sub_com == 'antispam':
         if args.sub_com_antispam == 'show':
--- ya360/logs.py
+++ ya360/logs.py
@@ -1,9 +1,8 @@
 """Модуль функций работы с API Yandex 360 журналми диска и почты"""
 
 import csv
-import json
+from pprint import pprint
 from yandex_360 import tools
-from datetime import datetime
 from .tid import load_token, load_orgid
 from .tools import check_request, nickname_to_uid, check_types
 
@@ -34,7 +33,7 @@ def show_mail_log(args):
 
     if args.afterDate:
         afterDate = args.afterDate
-    
+
     if args.types:
         types = args.types.split(',')
         check_types(types)
@@ -48,8 +47,46 @@ def show_mail_log(args):
             for ln in res:
                 writer.writerow(ln.values())
     else:
-        from pprint import pprint
+        pprint(res)
 
+    return None
+
+def show_disk_log(args):
+    """Функция возвращает список событий в аудит-логе Диска организации
+	
+	:param args: словарь аргументов командной строки
+	:type args: dict
+	"""
+
+    __token__ = load_token()
+    __orgid__ = load_orgid()
+
+    beforeDate=None
+    afterDate=None
+    includeUids=None
+    excludeUids=None
+
+    if args.includeUsers:
+        includeUids = nickname_to_uid(__token__,__orgid__, args.includeUsers.split(','))
+
+    if args.excludeUsers:
+        excludeUids = nickname_to_uid(__token__,__orgid__, args.excludeUsers.split(','))
+
+    if args.beforeDate:
+        beforeDate = args.beforeDate
+
+    if args.afterDate:
+        afterDate = args.afterDate
+
+    res = check_request(tools.get_disk_log(__token__,__orgid__,beforeDate,afterDate,includeUids,excludeUids))['events']
+
+    if args.csv:
+        with open(args.csv, 'w', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile, dialect='excel')
+            writer.writerow(res[0].keys())
+            for ln in res:
+                writer.writerow(ln.values())
+    else:
         pprint(res)
 
     return None
